5335c83efd055316f4649f0cd84934ade3135f42
discool <80698024+discool@users.noreply.github.com>
2021-04-16 10:33:17 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -34,21 +34,21 @@ game.onUpdateInterval(1000, function () {
     info.changeScoreBy(1)
     rock = sprites.create(img`
         . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . c c c c . . 
-        . c c c c c . c c c c c f c c . 
-        c c a c c c c c 8 f f c f f c c 
-        c a f a a c c a f f c a a f f c 
-        c a 8 f a a c a c c c a a a a c 
-        c b c f a a a a a c c c c c c c 
-        c b b a a c f 8 a c c c 8 c c c 
-        . c b b a b c f a a a 8 8 c c . 
-        . . . . a a b 1 b a a 8 a c . . 
-        . . . . c b c a a c c b . . . . 
-        . . . . b b c c a b b a . . . . 
-        . . . . b b a b a 6 a . . . . . 
-        . . . . c b b b 6 6 c . . . . . 
-        . . . . . c a 6 6 b c . . . . . 
-        . . . . . . . c c c . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . b b b b b f f f f f f . . . . 
+        b b 6 6 6 b b f f f f f f f . . 
+        b b 6 6 6 b b f f f f f f f . . 
+        . b b b b b f f f f f f . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
     rock.x += randint(-90, 65)
     rock.y += randint(-90, 65)
